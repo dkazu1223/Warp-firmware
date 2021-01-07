072d246ca80c6693c0df3edece552e7f1c67dca2
dkazu1223 <72918137+dkazu1223@users.noreply.github.com>
2021-01-07 17:47:02 +0000

    Update warp-kl03-ksdk1.1-boot.c

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -990,8 +990,8 @@ printPinDirections(void)
 //initiate lptmr
 
 #define LPTMR_INSTANCE 0
-/*
-uint32_t LPTMR_INSTANCE = 0;
+
+//uint32_t LPTMR_INSTANCE = 0;
 	
 static lptmr_state_t gLPTMRState;
 	
@@ -1006,10 +1006,9 @@ lptmr_user_config_t lptmrUserConfig =
 	
 	
 	
-LPTMR_DRV_Init(LPTMR_INSTANCE, &lptmrUserConfig, &gLPTMRState);
+//LPTMR_DRV_Init(LPTMR_INSTANCE, &lptmrUserConfig, &gLPTMRState);
 //LPTMR_DRV_Init(LPTMR_INSTANCE,&lptmrUserConfig,&gLPTMRState);
 
-*/
 
 void
 dumpProcessorState(void)
