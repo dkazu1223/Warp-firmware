9ba11a683d1d8bdf04ee510561c73b0f14a3eda2
dkazu1223 <72918137+dkazu1223@users.noreply.github.com>
2021-01-09 22:18:51 +0000

    Update warp-kl03-ksdk1.1-boot.c

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -1123,7 +1123,7 @@ for(int q=0; q<30; q++)
 	if (disp_time != disp_time_stored)
 		{
 		
-		devSSD1331symbols(6969,1,3);
+		//devSSD1331symbols(6969,1,3);
 		
 		//cleartest
 		
@@ -1139,7 +1139,7 @@ for(int q=0; q<30; q++)
 		if(td4 != td4s)
 			{
 				//devSSD1331symbols(6969,7,3);	
-				devSSD1331symbols(td4,7,3);
+				//devSSD1331symbols(td4,7,3);
 				td4s = td4;
 			}
 		
@@ -1147,7 +1147,7 @@ for(int q=0; q<30; q++)
 		if(td3!=td3s)
 			{
 				//devSSD1331symbols(6969,8,3);
-				devSSD1331symbols(td3,8,3);
+				//devSSD1331symbols(td3,8,3);
 				td3s = td3;
 			}
 		td5=disp_time-td6*60;
@@ -1155,7 +1155,7 @@ for(int q=0; q<30; q++)
 		if(td2!=td2s)
 			{
 				//devSSD1331symbols(6969,10,3);
-				devSSD1331symbols(td2,10,3);
+				//devSSD1331symbols(td2,10,3);
 				td2s = td2;
 			}
 		
@@ -1163,7 +1163,7 @@ for(int q=0; q<30; q++)
 		if(td1!=td1s)
 			{
 				//devSSD1331symbols(6969,11,3);
-				devSSD1331symbols(td1,11,3);				
+				//devSSD1331symbols(td1,11,3);				
 				td1s = td1;
 			}
 		
