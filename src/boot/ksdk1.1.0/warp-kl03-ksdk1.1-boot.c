705ca08d26e2e1fbd8ca659ab1a3c2e2ffc99f1c
dkazu1223 <72918137+dkazu1223@users.noreply.github.com>
2021-01-09 22:22:06 +0000

    Update warp-kl03-ksdk1.1-boot.c

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -1162,7 +1162,7 @@ for(int q=0; q<30; q++)
 		td1 = td5-td2*10;
 		if(td1!=td1s)
 			{
-				//devSSD1331symbols(6969,11,3);
+				devSSD1331symbols(6969,11,3);
 				devSSD1331symbols(td1,11,3);				
 				td1s = td1;
 			}
