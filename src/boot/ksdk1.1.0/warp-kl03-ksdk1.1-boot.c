e0b2f46a4038fec253491e115d1a904fedfac6f7
dkazu1223 <72918137+dkazu1223@users.noreply.github.com>
2021-01-12 14:13:16 +0000

    Update warp-kl03-ksdk1.1-boot.c

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -1154,7 +1154,7 @@ for(int q=0; q<400; q++)
 	// add to array
 	SEGGER_RTT_printf(0, "\r\tlargest acc %d\n", largestxyz);
 	accdata[q] = largestxyz;
-	filtdata[q] = 2*largestxyz/5
+	filtdata[q] = 2*largestxyz/5;
 	SEGGER_RTT_printf(0, "\r\taccdata %d\n", accdata[q]);
 	largestxyz = 0;
 	/*
