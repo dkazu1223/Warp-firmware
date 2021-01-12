8d6efa95a970f950abb6c5a22da8a261310381a0
dkazu1223 <72918137+dkazu1223@users.noreply.github.com>
2021-01-12 14:21:52 +0000

    Update warp-kl03-ksdk1.1-boot.c

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -1164,7 +1164,7 @@ for(int q=0; q<400; q++)
 		}
 	else
 		{
-		filtdata[q] = 2*largestxyz/5 + 10000;
+		filtdata[q] = 2*largestxyz/5;
 		}
 	
 	SEGGER_RTT_printf(0, "\r\taccdata %d\n", filtdata[q]);
