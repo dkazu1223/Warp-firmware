03ee6b29f6944264f0aaafa13174126bb4960b95
dkazu1223 <72918137+dkazu1223@users.noreply.github.com>
2021-01-13 14:57:51 +0000

    Update devSSD1331.c

--- src/boot/ksdk1.1.0/devSSD1331.c
+++ src/boot/ksdk1.1.0/devSSD1331.c
@@ -1745,7 +1745,7 @@ d4 = aut/1000;
 if (d4 != 0)
 {
 	df=1;
-	//devSSD1331symbols(6969,xco-3,yco);
+	devSSD1331symbols(6969,xco-3,yco);
 	devSSD1331symbols(d4,xco-3,yco);
 }
 d3 = (aut-d4*1000)/100;		
